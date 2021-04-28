9c0ac782d161c0fa5e2c2427393abac83a7408bc
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2021-04-28 15:19:15 -0400

    minor improvements in : PipelineManager Minor changes in GUIs. MainGUI: added option for manual deletion of files.

--- subFunc/deleteDatFile.m
+++ subFunc/deleteDatFile.m
@@ -1,6 +1,6 @@
 function deleteDatFile(file, FilePtr)
 % DELETEDATFILE deletes a file from a folder and from the file pointer.
-% Inputs: 
+% Inputs:
 % file : full path of .DAT file to be deleted.
 % FilePtr: full path to file pointer JSON file listing the .DAT file to be
 % deleted.
@@ -9,11 +9,11 @@ info = readPtr(FilePtr);
 idx = strcmp([filename,ext], {info.Files.Name});
 info.Files(idx) = [];
 if sum(idx) == 1
-delete(file);
-delete(strrep(file,'.dat', '_info.mat'));
-disp('File deleted!')
-write2Ptr(info,FilePtr);
-elseif sum(idx) == 0 
+    delete(file);
+    delete(strrep(file,'.dat', '_info.mat'));
+    disp('File deleted!')
+    write2Ptr(info,FilePtr);
+elseif sum(idx) == 0
     disp('File not found in File pointer')
 else
     disp('More than one file with the same name was found in File Pointer. Fix this and try again!')
