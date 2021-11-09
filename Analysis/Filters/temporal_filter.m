badd3e2d98fb8b1ce793d29c498e8dc3c359d205
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2021-11-09 15:35:32 -0500

    refact: Simplified temporal filter functions. Now temporal band-pass filtering and DeltaF/F calculation can be executed with "temporal_filter.m" fcn. More documentation is needed.

--- Analysis/Filters/temporal_filter.m
+++ Analysis/Filters/temporal_filter.m
@@ -1,4 +1,14 @@
-function outFile = cheby1_BPtemporalFilter(File, SaveFolder, varargin)
+function outFile = temporal_filter(File, SaveFolder, varargin)
+% This function applies a 4th-order Butterworth filter to the Time dimension of
+% imaging data. This is achieved by the subtraction of two low-pass
+% filters.
+
+
+
+% The data must be an Image time series with dimensions
+% {Y,X,T}.
+% This function is a wrapper of the IOI library function
+% "NormalisationFiltering.m".
 
 %%% Arguments parsing and validation %%%
 p = inputParser;
@@ -8,10 +18,14 @@ addRequired(p,'File',@isfile)% For a file as input.
 addRequired(p, 'SaveFolder', @isfolder);
 % Optional Parameters:
 % opts structure
-default_opts = struct('LowPassHz', .3, 'HighPassHz', 3);
+default_opts = struct('LowCutOffHz', .0033, 'HighCutOffHz', -1, 'Normalize', true); 
+% Some notes on the CutOff values:
+    % 1) The HighCutOffHz value of -1 will be translated as the Nyquist of the sample rate
+    % 2) Other than the case above, all cut-off values equal or less than
+    % zero will disable the Low-pass filter. 
 addOptional(p, 'opts', default_opts,@(x) isstruct(x) && ~isempty(x));
 % Output File:
-default_Output = 'cheby1Filt.dat';
+default_Output = 'BPtemporalFilter.dat';
 addOptional(p, 'Output', default_Output, @(x) ischar(x) || isstring(x) || iscell(x));
 % Parse inputs:
 parse(p,File, SaveFolder, varargin{:});
@@ -22,26 +36,20 @@ SaveFolder = p.Results.SaveFolder;
 opts = p.Results.opts;
 Output = p.Results.Output;
 %%%%
-% memmap DataFile and MetaDataFile:
+
+% memory map DataFile and MetaDataFile:
 [mDat, mDt] = mapDatFile(File);
-Freq = mDt.Freq;
-% Locate "T"ime dimension:
-indxT = strcmp('T', mDt.dim_names);
-% Initialize Chebyshev Filter Parameters:
-bp = [opts.LowPassHz opts.HighPassHz]/(Freq/2); % Divide LowPass and HighPass values by Nyquist.
-[b,a] = cheby1(2,0.5,bp);
-% Load data:
+if opts.HighCutOffHz == -1
+    opts.HighCutOffHz = mDt.Freq/2; % Translate "-1" to Nyquist.
+end
+% Load Data:
 data = mDat.Data.data;
 % Find NaNs and replace them with zeros:
 idx_nan = isnan(data);
 data(idx_nan) = 0;
-% Filter:
-szData = size(data);
-data = reshape(data,[], szData(indxT));
-for i = 1:size(data,1)
-    data(i,:) = single(filtfilt(b,a,double(data(i,:))));
-end
-data = reshape(data,szData);
+% Run Temporal filter function
+data = NormalisationFiltering(data, mDt.Freq, opts.LowCutOffHz, opts.HighCutOffHz, ...
+    opts.Normalize);
 % Put NaNs back to data:
 data(idx_nan) = NaN;
 % SAVING DATA :
@@ -51,5 +59,3 @@ datFile = fullfile(SaveFolder, Output);
 save2Dat(datFile, data, mDt.dim_names);
 outFile = Output;
 end
-
-
