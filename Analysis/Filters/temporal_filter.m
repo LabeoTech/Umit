f6dee34a75b63e5d2e7970304990849ec43f20f5
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2021-11-10 16:08:11 -0500

    refact: updated temporal_filter fcn documentation.

--- Analysis/Filters/temporal_filter.m
+++ Analysis/Filters/temporal_filter.m
@@ -1,14 +1,23 @@
 function outFile = temporal_filter(File, SaveFolder, varargin)
-% This function applies a 4th-order Butterworth filter to the Time dimension of
-% imaging data. This is achieved by the subtraction of two low-pass
-% filters.
+% This function applies a band-pass 4th-order Butterworth filter to the Time
+% dimension of an image time series (Y,X,T) dataset. 
+% The filtering algorithm consists in creating two low-passed versions of the 
+% signal with a given cut-off frequency ("LowCutOff" and "HighCutOff") and 
+% subsequently subtracting the two filtered signals. 
+% In this case, the signal(R) is expressed as DeltaR.
+% Optionally, the subtracted signals can be normalized to the low cut-off signal 
+% to express the signal as DeltaR/R.
 
-
-
+% Limitations:
 % The data must be an Image time series with dimensions
 % {Y,X,T}.
-% This function is a wrapper of the IOI library function
-% "NormalisationFiltering.m".
+
+% This function is a wrapper of the IOI library function "NormalisationFiltering.m".
+% For more information on the algorithm, refer to the function's documentation.
+
+
+
+
 
 %%% Arguments parsing and validation %%%
 p = inputParser;
@@ -18,11 +27,10 @@ addRequired(p,'File',@isfile)% For a file as input.
 addRequired(p, 'SaveFolder', @isfolder);
 % Optional Parameters:
 % opts structure
-default_opts = struct('LowCutOffHz', .0033, 'HighCutOffHz', -1, 'Normalize', true); 
+default_opts = struct('LowCutOffHz', .0033, 'HighCutOffHz', 0, 'Normalize', true); 
 % Some notes on the CutOff values:
-    % 1) The HighCutOffHz value of -1 will be translated as the Nyquist of the sample rate
-    % 2) Other than the case above, all cut-off values equal or less than
-    % zero will disable the Low-pass filter. 
+    % 1) The HighCutOffHz value of 0 will be translated as the Nyquist of the sample rate
+    % 2) For the LowCutOff, values equal or less than zero will give a low-passed signal at "HighCutOff". 
 addOptional(p, 'opts', default_opts,@(x) isstruct(x) && ~isempty(x));
 % Output File:
 default_Output = 'BPtemporalFilter.dat';
@@ -38,6 +46,11 @@ Output = p.Results.Output;
 %%%%
 % memory map DataFile and MetaDataFile:
 [mDat, mDt] = mapDatFile(File);
+% Validata if Data is an Image Time Series:
+if ~all(ismember(mDt.dim_names, {'Y', 'X', 'T'}))
+    error('umIToolbox:temporal_filter:InvalidInput', ...
+        'Wrong Input Data type. Data must be an Image time series with dimensions "X", "Y" and "T".');
+end
 if opts.HighCutOffHz == -1
     opts.HighCutOffHz = mDt.Freq/2; % Translate "-1" to Nyquist.
 end
