3c16ce60f9bef0e3084cd89fd2ae62eadfc88be2
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2021-12-14 10:16:22 -0500

    removed overloading fcns. Not worth the trouble.

--- @char/isfile.m
+++
@@ -1,9 +0,0 @@
-function tf = isfile(fileName)
-% Function to overload Matlab's built-in function "isfolder". For backward
-% compatobility with versions prior to R2019a.
-tf = false;
-if exist(fileName, 'file')
-    tf = true;
-end
-end
-
