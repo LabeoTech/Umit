635dac656773138669f8af6fc2800d7fbe1bbafb
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-02-09 16:26:40 -0500

    dev: added extra option to output file as full path or not.

--- subFunc/getNamesFromDir.m
+++ subFunc/getNamesFromDir.m
@@ -1,8 +1,8 @@
 function names = getNamesFromDir(Folder, exp, isDir, varargin)
 % This function extracts the names of elements from a structure obtained
 % using the built-in "dir" function.
-%   Inputs are the path "Folder" where the elements are, a regular expression "exp"
-%   and a boolean "isDir", with true if the element is a directory.
+% Inputs are the path "Folder" where the elements are, a regular expression "exp"
+% and a boolean "isDir", with true if the element is a directory.
 
 % Arguments validation
 p = inputParser;
@@ -13,10 +13,14 @@ validate_isDir = @(x) isnumeric(x) || islogical(x);
 addRequired(p, 'isDir', validate_isDir)
 validate_flag = @(x) mustBeMember(x,{'match', 'tokens'});
 addOptional(p, 'flag', 'match', validate_flag);
+addOptional(p,'fullpath', true, @islogical)
 parse(p,Folder,exp,isDir,varargin{:});
-%%%%%%
+%%%%%
 names = dir(p.Results.Folder);
 names = {names([names.isdir] == p.Results.isDir).name};
 names = regexp(names, p.Results.exp, p.Results.flag);
 names = [names{:}];
+if p.Results.fullpath
+    names = fullfile(Folder,names);
+end
 end
