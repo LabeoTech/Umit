2a48220add6901a8abe9c5c57c6616017352dc30
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-02-16 15:14:15 -0500

    new: Added wrapper function for Ana_Speckle fcn from IOI_ana.

---
+++ Analysis/Imaging/run_Ana_Speckle.m
@@ -0,0 +1,25 @@
+function outFile = run_Ana_Speckle(SaveFolder)
+% RUN_ANA_SPECKLE calls the function ANA_SPECKLE from the IOI library (LabeoTech).
+% In brief, this function calculates blood flow (in arbitrary units) from 
+% a Laser Speckle Contrast Imaging data
+
+% Defaults:
+default_Output = {'Flow.dat'}; % This line is here just for Pipeline management...
+                               % In this case, the string must be inside a
+                               % in order to PipelineManager to work.
+
+%%% Arguments parsing and validation %%%
+p = inputParser;
+% Save folder:
+addRequired(p, 'SaveFolder', @isfolder);
+% Parse inputs:
+parse(p,SaveFolder);
+SaveFolder = p.Results.SaveFolder;
+clear p
+
+% Run Ana_Speckle function from IOI library:
+disp('Calculating  blood flow...')
+Ana_Speckle(SaveFolder);
+outFile = default_Output;
+disp('Finished Speckle Mapping.')
+end
