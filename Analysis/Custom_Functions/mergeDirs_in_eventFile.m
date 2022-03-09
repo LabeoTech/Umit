b51f7a9cf07051798946eeed0a8283199db661cd
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-03-09 11:08:19 -0500

    updated to compatibility with new version of umIT

--- Analysis/Custom_Functions/mergeDirs_in_eventFile.m
+++ Analysis/Custom_Functions/mergeDirs_in_eventFile.m
@@ -1,22 +1,21 @@
-function mergeDirs_in_eventFile(File, SaveFolder)
+function mergeDirs_in_eventFile(SaveFolder)
 % MERGEDIRS_IN_EVENTFILE modifies the variables in events.mat file 
 % by merging the grating directions as "repetitions" of the same stimulus.
 % This is a custom function used in Matthieu Vanni's lab.
 
 % Inputs:
-%   File: This is a dummy variable so the function can work!! No changes
-%   are applied to the file's metadata.
 %   SaveFolder: directory to save .MAT eventsfile.
 
 %%% Arguments parsing and validation %%%
 p = inputParser;
-addRequired(p,'File',@isfile)
 addRequired(p, 'SaveFolder', @isfolder);
 % Parse inputs:
-parse(p,File, SaveFolder);
+parse(p, SaveFolder);
 %Initialize Variables:
 SaveFolder = p.Results.SaveFolder;
+clear p
 %%%%
+disp('Merging Directions...')
 % Check if events.mat exists:
 evntFile = fullfile(SaveFolder, 'events.mat');
 if ~isfile(evntFile)
@@ -52,4 +51,5 @@ new_eventNameList = cellfun(@(x) num2str(x), new_eventNameList, 'UniformOutput',
 mD.Properties.Writable = true;
 mD.eventID = repelem(newID_list,2); % Replace IDs for state == 0
 mD.eventNameList = new_eventNameList;
+disp('Events file updated!')
 end
