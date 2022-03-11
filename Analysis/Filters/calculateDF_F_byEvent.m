ab383c1ee24afb3ba6b3eb5edcd9599b16a12957
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-03-11 13:29:21 -0500

    made detrending optional in "calculateDF_F_byEvent.m" analysis fcn.

--- Analysis/Filters/calculateDF_F_byEvent.m
+++ Analysis/Filters/calculateDF_F_byEvent.m
@@ -1,24 +1,30 @@
-function outData = calculateDF_F_byEvent(data, metaData)
+function outData = calculateDF_F_byEvent(data, metaData, varargin)
 % CALCULATEDF_F_BYEVENT generates DeltaF/F0 movie from DATA.
 % The data must be a 4-D matrix containing image time series separated by
 % trials.
 % Inputs:
 %   data (4D numerical matrix): Image time series separated by events with dimensions {'E','Y','X','T}.
 %   metaData: .mat file with meta data associated with "data".
+%   opts (struct): opts structure containing :
+%       b_applyDetrend (bool): If true, applies "detrend" function over
+%       time.
 % Output:
 %   outData(4D numerical matrix): "data" with values transformed to express
 %   DeltaF/F0.
 % Defaults:
 default_Output = 'deltaF_F_ByEvent.dat'; %#ok This is here only as a reference for PIPELINEMANAGER.m. 
+default_opts = struct('b_applyDetrend',false);
 %%% Arguments parsing and validation %%%
 p = inputParser;
 addRequired(p,'data',@(x) isnumeric(x)); % Validate if the input is a 3-D numerical matrix:
 addRequired(p,'metaData', @(x) isa(x,'matlab.io.MatFile') | isstruct(x)); % MetaData associated to "data".
+addOptional(p, 'opts', default_opts, @isstruct);
 % Parse inputs:
-parse(p,data, metaData);
+parse(p,data, metaData, varargin{:});
 %Initialize Variables:
 outData = p.Results.data; 
 metaData = p.Results.metaData;
+opts = p.Results.opts;
 clear p
 %%%%
 % Validate if the data is an Image time series by Events:
@@ -29,13 +35,19 @@ assert(isequal(metaData.dim_names, {'E','Y','X','T'}), errID, errMsg)
 bsln = median(outData(:,:,:,1:round(metaData.preEventTime_sec*metaData.Freq)), ...
     4,'omitnan');
 % Perform linear detrending on data:
-szdat = size(outData);
-outData = reshape(outData,prod(szdat(1:3)),[])';
-disp('Detrending...');
-outData = detrend(outData,1, 'Continuous', false); % Here, we remove linear trends from each trial.
-outData = reshape(outData',szdat);
-% Normalize data to get DeltaF/F values
-disp('Calculating DeltaF/F ...');
-outData = bsxfun(@rdivide,outData,bsln);
+if opts.b_applyDetrend
+    szdat = size(outData);
+    outData = reshape(outData,prod(szdat(1:3)),[])';
+    disp('Detrending...');
+    outData = detrend(outData,1, 'Continuous', false); % Here, we remove linear trends from each trial.
+    outData = reshape(outData',szdat);
+    % Normalize data to get DeltaF/F values
+    disp('Calculating DeltaF/F ...');
+    outData = 1 + bsxfun(@rdivide,outData,bsln);
+else
+    % Normalize data to get DeltaF/F values
+    disp('Calculating DeltaF/F ...');
+    outData = 1 + bsxfun(@rdivide,bsxfun(@minus,outData,bsln),bsln);
+end
 disp('Done!')
 end
