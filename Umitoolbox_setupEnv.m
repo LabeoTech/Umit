139786b49121b9a0116743c9a66e62c0f82b4017
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-06-07 14:13:58 -0400

    dev:Umitoolbox_setupEnv: added code to access local documentation when creating env. var. refact: Umitoolbox_setupEnv: minor refactoring.

--- Umitoolbox_setupEnv.m
+++ Umitoolbox_setupEnv.m
@@ -4,25 +4,34 @@ function Umitoolbox_setupEnv
 % toolbox is located. This is used by the GUI scripts to find folders and
 % files in the computer.
 
-saveDir = uigetdir(pwd, 'Select Toolbox Folder');
-if saveDir == 0
-    disp('Operation cancelled by User')
-    return
-end
-sys = computer;
 myenv = getenv('Umitoolbox');
 if isfolder(myenv)
     disp('Environment Variable Umitoolbox already exists!')
 else
+    saveDir = uigetdir(pwd, 'Select Toolbox Folder');
+    if saveDir == 0
+        disp('Operation cancelled by User')
+        return
+    end
+    sys = computer;
     switch sys
         case 'PCWIN64'
             system(['SETX Umitoolbox ' saveDir]);
             disp('done!');
-            disp('Restart MATLAB to apply the changes!');
+            disp('Restart MATLAB to apply the changes and rerun this function!');
         otherwise
             disp('For this computer, you have to set manually the environment variable "Umitoolbox"');
     end
 end
-% Add toolbox to savePath:
-addpath(genpath(saveDir)); savepath
+% Edit info.xml file to be able to access the documentation through Matlab "doc":
+info_file = fileread(fullfile(myenv,'html','info.xml'));
+path_str = regexp(info_file,'(?<=<help_location>)(\S+)(?=</help_location>)', 'match');
+% Replace default path string with user's path:
+info_file = strrep(info_file,path_str{:}, fullfile(myenv,'html'));
+% Save info_file:
+fid = fopen(fullfile(myenv,'html','info.xml'),'w');
+fprintf(fid,'%s',info_file);
+fclose(fid);
+% Add toolbox to Path:
+addpath(genpath(saveDir)); 
 end
