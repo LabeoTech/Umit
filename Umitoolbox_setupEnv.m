6efd21300dca768b11dc4e5240e97c5e240efbdc
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-07-14 13:29:34 -0400

    dev: umIToolbox_setupEnv: automatically removes existing helpsearch folder before creating a new one to avoid errors.

--- Umitoolbox_setupEnv.m
+++ Umitoolbox_setupEnv.m
@@ -38,6 +38,9 @@ fid = fopen(fullfile(myenv,'docs','info.xml'),'w');
 fprintf(fid,'%s',info_file);
 fclose(fid);
 % Build serch database:
+folders = dir(fullfile(myenv, 'docs'))
+help_dir = folders([folders.isdir] & startsWith({folders.name}, 'helpsearch-v'));
+status = rmdir(fullfile(help_dir.folder, help_dir.name), 's');
 builddocsearchdb(fullfile(myenv,'docs'))
 % Add toolbox to Path for the current Matlab session:
 addpath(genpath(getenv('Umitoolbox')));
