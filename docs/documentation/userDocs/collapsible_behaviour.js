e4c5689abab7d8522fb54206d46566915a99f336
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-08-17 16:46:59 -0400

    added first analysis fcn doc.

---
+++ docs/documentation/userDocs/collapsible_behaviour.js
@@ -0,0 +1,18 @@
+
+// This script collapses and expands a section by clicking on it.
+// This is used in the "optional parameters" section of the umIT's functions documentation.
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log(content);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
