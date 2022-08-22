d82c4a41a0e50abd7eeddc676d214da501ea38b5
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-08-22 15:52:45 -0400

    docs: added content; dev: spatialGaussFilt: accepts only image time series now; removed option to set Filter Kernel size. Added error message for invalid sigma value. Forced filter to be done in "spatial" domain.

--- Analysis/Filters/spatialGaussFilt.m
+++ Analysis/Filters/spatialGaussFilt.m
@@ -5,12 +5,12 @@ function outData = spatialGaussFilt(data, metaData, varargin)
 
 % Limitations:
 % The data must be an Image time series OR a Correlation map with
-% dimensions: {Y,X,T or S}.
+% dimensions: {Y,X,T}.
 
 % Defaults:
 default_Output = 'spatFilt.dat'; %#ok. This line is here just for Pipeline management.
-default_opts = struct('KernelSize', 5,'Sigma',1);
-opts_values = struct('KernelSize', [1:2:51], 'Sigma',[0,Inf]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
+default_opts = struct('Sigma',1);
+opts_values = struct('Sigma',[0,Inf]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
 %%% Arguments parsing and validation %%%
 p = inputParser;
 % The input of the function must be a File , RawFolder or SaveFolder
@@ -26,18 +26,17 @@ opts = p.Results.opts;
 clear p
 %%%%
 % Validate if "data" is an Image Time Series:
-errID = 'umIToolbox:GSR:InvalidInput';
+errID = 'umIToolbox:spatialGaussFilt:InvalidInput';
 errMsg = ['Wrong Input Data type. Data must be an Image time series with ' ...
-    'dimensions "X", "Y", "T" or  a correlation map  with dimensions "X","Y","S".'];
-assert(all(ismember(metaData.dim_names,{'Y', 'X', 'T'})) || ...
-    all(ismember(metaData.dim_names,{'Y', 'X', 'S'})), errID, errMsg);
-
+    'dimensions "X", "Y", "T".'];
+assert(all(ismember(metaData.dim_names,{'Y', 'X', 'T'})), errID, errMsg);
+assert(opts.Sigma>0, errID,'Sigma must be a positive value!');
 % Apply gaussian filter:
 disp('Filtering frames...')
 w = waitbar(0,'Filtering frames...');
 for i = 1:size(outData,3)
     waitbar(i/size(outData,3),w);
-    outData(:,:,i) = imgaussfilt(outData(:,:,i), opts.Sigma,'FilterSize', opts.KernelSize);
+    outData(:,:,i) = imgaussfilt(outData(:,:,i), opts.Sigma, 'FilterDomain', 'spatial'); % Forced FilterDomain to "spatial" to avoid problems when the data has Infs or NaNs.);
 end
 close(w)
 disp('Finished with Spatial Gaussian filter.')      
