8121572c249c94508a7cf7fd870764db0485ea4f
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-09-12 16:16:14 -0400

    dev: started fcn for Maps averaging.

---
+++ GUI/StatsModule/avgMaps.m
@@ -0,0 +1,22 @@
+function avgData = avgMaps(statsobj, b_useMsk, b_isPolar, b_scale)
+% AVGMAPS creates an average map (i.e. image with dimensions Y,X) per group
+% per acquisition.
+
+disp('creating average plots')
+% Separate data by groups and aquisitions.
+
+% Perform translation and scaling.
+
+% average maps
+
+% use mask to hide pixels not from ROI.
+
+
+
+
+
+
+
+
+end
+
