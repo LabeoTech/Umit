cdc178ba7f2f7ad915a9700c66cfe479aa6b38fb
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-10-03 12:30:05 -0400

    dev: normalizeBSLN: now the function accepts image time series. dev: run_HemoCompute: updated function following changes in HemoCompute fcn. docs: run_HemoCompute, apply_detrend, normalizeBSLN: updated docs.

--- Analysis/Filters/normalizeBSLN.m
+++ Analysis/Filters/normalizeBSLN.m
@@ -1,15 +1,24 @@
 function [outData, metaData] = normalizeBSLN(data, metaData, varargin)
-% NORMALIZEBSLN normalizes event-triggered data by a baseline. Here, a
-% baseline corresponds to the median of the pixel values before the event
-% (pre-event time).
-% The data must be a 4-D matrix containing image time series separated by
-% trials.
+% NORMALIZEBSLN normalizes event-triggered and image time series data by a baseline.
+% Here, a baseline corresponds to the median of the pixel values before the event
+% (pre-event time) or a value in seconds stored in the variable "baseline_sec".
+% If the data is an image time series, the baseline value "auto" corresponds 
+% to the 20% first frames of the data.
+% The data must be a 3-D matrix containg image time series or a 4-D matrix 
+% containing image time series separated by trials.
 % Inputs:
-%   data (4D numerical matrix): Image time series separated by events with dimensions {'E','Y','X','T}.
-%   metaData (struct): structure containing smeta data associated with "data".
-%   opts (optional) : structure containing extra parameters.
+%   data (3D or 4D numerical matrix): Image time series or image time series separated by events.
+%   metaData (struct): structure containing meta data associated with "data".
+%   opts (optional) : structure containing extra parameters:
+%       baseline_sec("auto" or a number): baseline period(in seconds). If
+%           set to "auto" the value stored in the metaData as
+%           "preEventTime_sec" will be applied. If the input data is a 3D
+%           matrix, the "auto" value corresponds to the 20% first frames of the
+%           data.
+%       b_centerAtOne (bool): Set to TRUE to center the normalized data at
+%           one. Otherwise, the data will be centered at zero.
 % Output:
-%   outData(4D numerical matrix): "data" with values transformed to express
+%   outData(3D or 4D numerical matrix): "data" with values transformed to express
 %   DeltaR/R0.
 %   metaData: .mat file with meta data associated with "outData".
 % Defaults:
@@ -30,22 +39,42 @@ opts = p.Results.opts;
 clear p
 %%%%
 % Validate if the data is an Image time series by Events:
-errMsg = 'Input Data must be an Image time series separated by events with dimensions {"E","Y","X",T"}.';
+errMsg = 'Input Data must be an Image time series with dimensions "Y","X",T" or "E","Y","X","T".';
 errID = 'umIToolbox:normalizeBSLN:WrongInput';
-assert(isequal(metaData.dim_names, {'E','Y','X','T'}), errID, errMsg)
-% 
-% Update event timings in metaData:
-if ~strcmpi(opts.baseline_sec,'auto')   
+assert(all(ismember({'Y','X','T'}, metaData.dim_names)), errID, errMsg)
+if strcmpi('E',metaData.dim_names)
     % Update event timings in metaData:
-    metaData.postEventTime_sec = metaData.postEventTime_sec - opts.baseline_sec;
-    metaData.preEventTime_sec = single(opts.baseline_sec);    
+    if ~strcmpi(opts.baseline_sec,'auto')
+        % Update event timings in metaData:
+        metaData.postEventTime_sec = metaData.postEventTime_sec - opts.baseline_sec;
+        metaData.preEventTime_sec = single(opts.baseline_sec);
+        bsln_sec = metaData.preEventTime_sec;
+    else
+        bsln_sec = metaData.pre_EventTime_sec;
+    end
+else
+   if strcmpi(opts.baseline_sec, 'auto')
+       bsln_sec = .2*size(outData,3)/metaData.Freq; % Set baseline as first 20% of the frames for image time series.
+       warning('Setting arbitrary baseline time of %0.2f seconds for image time series input.', bsln_sec);
+   else
+       bsln_sec = single(opts.baseline_sec);
+   end              
 end
-% Calculate baseline:
-bsln = median(outData(:,:,:,1:round(metaData.preEventTime_sec*metaData.Freq)), ...
-    4,'omitnan');
-% Normalize data from baseline:
+
 disp('Normalizing data by baseline...');
+% Calculate baseline:
+if strcmpi('E',metaData_dim_names)
+    % for 4D data:
+    bsln = median(outData(:,:,:,1:round(bsln_sec*metaData.Freq)), ...
+        4,'omitnan');
+else
+    % for 3D data:
+    bsln = median(outData(:,:,1:round(bsln_sec*metaData.Freq)), ...
+        3,'omitnan');
+end
+% Normalize data from baseline:
 outData = (outData - bsln)./bsln;
+% Center data at ONE:
 if opts.b_centerAtOne
     outData = outData + 1;
 end
--- Analysis/Imaging/run_HemoCompute.m
+++ Analysis/Imaging/run_HemoCompute.m
@@ -1,11 +1,11 @@
 function outFile = run_HemoCompute(RawFolder,SaveFolder, varargin)
 % RUN_HEMOCOMPUTE calls the function HEMOCOMPUTE from the IOI library (LabeoTech).
-
+% For more info, read the HEMOCOMPUTE docstring.
 
 % Defaults:
 default_Output = {'HbO.dat', 'HbR.dat'}; % This line is here just for Pipeline management.
-default_opts = struct('FilterSet', 'GCaMP', 'Red', true, 'Green', true, 'Amber', true);
-opts_values = struct('FilterSet',{{'GCaMP', 'jRGECO' , 'none'}},'Red',[false, true], 'Green',[false, true],'Amber',[false, true]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
+default_opts = struct('FilterSet', 'GCaMP', 'b_normalize', true, 'Red', true, 'Green', true, 'Amber', true);
+opts_values = struct('FilterSet',{{'GCaMP', 'jRGECO' , 'none'}},'b_normalize',[true,false],'Red',[false, true], 'Green',[false, true],'Amber',[false, true]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
 %%% Arguments parsing and validation %%%
 p = inputParser;
 % Save folder:
@@ -22,13 +22,13 @@ outFile = default_Output;
 clear p
 % Translate opts to char cell array:
 fields = fieldnames(opts);
-fields = setdiff(fields, 'FilterSet');
+fields = setdiff(fields, {'FilterSet', 'b_normalize'});
 idx = cellfun(@(x) opts.(x), fields);
 list = fields(idx)';
 
 % Run HemoCompute function from IOI library:
 try
-    HbO = HemoCompute(RawFolder,SaveFolder, opts.FilterSet, list); %#ok The output is here just to catch assertion errors from HemoCompute.
+    HbO = HemoCompute(RawFolder,SaveFolder, opts.FilterSet, list, opts.b_normalize); %#ok The output is here just to catch assertion errors from HemoCompute.
 catch ME
     ME = addCause(ME, MException('umIToolbox:run_HemoCompute:UnkwnownError',...
         'Function HemoCompute Failed! Check Matlab command window for messages!'));
