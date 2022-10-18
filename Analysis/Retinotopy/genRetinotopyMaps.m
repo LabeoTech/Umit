b4b58e19c088dc2af2cf3047d2db7f26e9963810
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-10-18 11:23:53 -0400

    dev: dimensions_names: added new dimension "F" for FFT dataset. dev: genRetinotopyMaps: updated code to use only events from "events.mat" file; added option to average and normalize trials. new: genVSM: added function to calculate and filter visual sign map (retinotopy).

--- Analysis/Retinotopy/genRetinotopyMaps.m
+++ Analysis/Retinotopy/genRetinotopyMaps.m
@@ -1,15 +1,10 @@
 function outFile = genRetinotopyMaps(data, metaData,SaveFolder, varargin)
 % GENRETINOTOPYMAPS create amplitude and phase maps for each cardinal
 % direction as well as the azimuth and elevation maps when at least two
-% perpendicular directions are present in the input data. 
+% perpendicular directions are present in the input data.
 %
-% Necessary info:
-%   The metaData file must contain the variable "Stim_trialMarker" containing
-%   the markers that identify the frames of each direction. In addition,
-%   the file must contain the variables "eventID" and "eventNameList" with the 
-%   indices and direction identifiers, respectively. The identifiers encoding one
-%   or more of the cardinal angles are: 0,90,180 and 270.
-%
+% The timestamps marking the individual directions must be encoded in the "events.mat" file.
+
 % Inputs:
 %   data: numerical matrix containing image time series (with dimensions "Y", "X", "T").
 %   metaData: .mat file with meta data associated with "data".
@@ -20,8 +15,8 @@ function outFile = genRetinotopyMaps(data, metaData,SaveFolder, varargin)
 
 % Defaults:
 default_Output = {'AzimuthMap.dat' 'ElevationMap.dat'};  %#ok. This line is here just for Pipeline management.
-default_opts = struct('nSweeps', 1);
-opts_values = struct('nSweeps', [1,Inf]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
+default_opts = struct('nSweeps', 1, 'b_useAverageMovie', false);
+opts_values = struct('nSweeps', [1,Inf], 'b_useAverageMovie', [true,false]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
 %%% Arguments parsing and validation %%%
 p = inputParser;
 addRequired(p,'data',@(x) isnumeric(x)); % Validate if the input is numerical
@@ -44,9 +39,10 @@ errID1 = 'umIToolbox:genRetinotopyMaps:WrongInput';
 errID2 = 'umIToolbox:genRetinotopyMaps:MissingInput';
 assert(all(ismember({'Y','X','T'},metaData.dim_names)), errID1,...
     'Input data must be an image time series with dimensions "Y","X","T".')
-% Check if the metaData has the necessary variables:
-assert(all(ismember({'Stim_trialMarker','eventID','eventNameList'},fieldnames(metaData))),errID2,...
-    'The one or more of the variables "Stim_trialMarker", "eventID" and "eventNameList" are missing in meta data file.')
+% Check if the "events.mat" file exists and if the event Names correspond to the directions 0, 90, 180 and 270:
+assert(isfile(fullfile(SaveFolder, 'events.mat')), errID2, '"events.mat" file not found!')
+evntInfo = load(fullfile(SaveFolder, 'events.mat'));
+assert(all(ismember(evntInfo.eventNameList, {'0','90','180','270'})), errID2, 'Invalid directions found in "events.mat" file! They must be one of the following: 0,90,180,270.')
 %%%%
 outFile = {};
 % Prepare data for FFT calculation:
@@ -55,59 +51,86 @@ outFile = {};
 % data(idx_nan) = 0;
 
 % Split data and calculate fft over time for each direction:
-ampMaps = cell(size(metaData.eventNameList));
+ampMaps = cell(size(evntInfo.eventNameList));
 phiMaps = ampMaps;
+% Select Frequency:
+if opts.b_useAverageMovie
+    freqFFT = 2;
+else
+    freqFFT = round(opts.nSweeps)+1;
+end
 
-fr_rise = find(metaData.Stim_trialMarker < .5 & [metaData.Stim_trialMarker(:,2:end) nan] > .5);
-fr_fall = find(metaData.Stim_trialMarker > .5 & [metaData.Stim_trialMarker(:,2:end) nan] < .5);
-freqFFT = round(opts.nSweeps)+1; 
 w = waitbar(0,'Calculating FFT ...', 'Name', 'genRetinotopyMaps');
 w.Children.Title.Interpreter = 'none';
-for i = 1:numel(metaData.eventNameList)
-    w.Children.Title.String = ['Calculating FFT for direction ' metaData.eventNameList{i}];
-    % Calculate FFT:
-    fDat = fft(data(:,:,fr_rise(i):fr_fall(i)),[],3);      
-    % Create Amplitude/Phase maps for the input frequency (from Zhuang et al., 2017)  
+framestamps = round(evntInfo.timestamps*metaData.Freq);
+for i = 1:numel(evntInfo.eventNameList)
+    w.Children.Title.String = ['Calculating FFT for direction ' evntInfo.eventNameList{i}]; drawnow
+    indxOn = find(evntInfo.eventID == i & evntInfo.state == 1);
+    indxOff = find(evntInfo.eventID == i & evntInfo.state == 0);
+    if opts.b_useAverageMovie        
+        % Create average DeltaR movie for each direction:
+        bsln_len = round(mean(framestamps(indxOn(2:end)) - framestamps(indxOff(1:end-1))));
+        if isempty(bsln_len) || bsln_len <= 0
+            error('Could not compute a valid baseline period! Does the recording have interstimulus time? If not, set the option "b_useAverageMovie" to FALSE.');
+        end
+        trial_len = round(mean(framestamps(indxOff) - framestamps(indxOn)));
+        avg_mov = zeros([metaData.datSize, trial_len + bsln_len],'single');
+        disp(['Creating average Delta R movie for direction ' evntInfo.eventNameList{i}]);
+        for ii = 1:length(indxOn)
+            DeltaR = data(:,:,framestamps(indxOn(ii)) - bsln_len : framestamps(indxOn(ii)) + trial_len -1) - ...
+                median(data(:,:,framestamps(indxOn(ii)) - bsln_len: framestamps(indxOn(ii)) - 1), 3,'omitnan'); % trial period minus the intertrial period.
+            avg_mov = [avg_mov + DeltaR];
+        end
+        avg_mov = avg_mov/length(indxOn); % Average DeltaR movie.
+        % Calculate FFT of average movie:
+        fDat = fft(avg_mov,[],3);
+    else
+        % Get all frames that the current stimulus was presented (i.e. exclude
+        % inter stim frames):
+        frOn = [];
+        for ii = 1:length(indxOn)
+            frOn = [frOn, framestamps(indxOn(ii)):framestamps(indxOff(ii))-1];
+        end
+        % Calculate FFT of whole stimulus:
+        fDat = fft(data(:,:,frOn),[],3);
+    end
+    % Create Amplitude/Phase maps for the input frequency (from Zhuang et al., 2017)
     ampMaps{i} = (abs(fDat(:,:,freqFFT)) * 2) / size(fDat,3);
     phiMaps{i} = mod(-1.*angle(fDat(:,:,freqFFT)),2*pi);
-    waitbar(i/numel(metaData.eventNameList),w);
+    waitbar(i/numel(evntInfo.eventNameList),w);
 end
-clear fDat md
+clear fDat md avg_mov i
 close(w);
 %%% Calculate Azimuth and Elevation maps
 % Check if all directions exist:
-AzimMap = zeros([metaData.datSize 1],'single');
+AzimMap = zeros([metaData.datSize 2],'single');
 ElevMap = AzimMap;
-[idxAz,indxAz] = ismember({'0','180'}, metaData.eventNameList);
-[idxEl,indxEl] = ismember({'90','270'}, metaData.eventNameList);
+[idxAz,indxAz] = ismember({'0','180'}, evntInfo.eventNameList);
+[idxEl,indxEl] = ismember({'90','270'}, evntInfo.eventNameList);
 % Azimuth:
 if all(idxAz)
     disp('Calculating Azimuth map...')
-    pow = mean(cat(3,ampMaps{indxAz}),3); % Average amplitude;
-    phi = (phiMaps{indxAz(1)} - phiMaps{indxAz(2)})/2; % From Kalatsky and Stryker, 2003
-    % Package amplitude and phase into complex numbers:
-    AzimMap(:,:) = pow.*(cos(phi) + i.*sin(phi));     
-end  
+    AzimMap(:,:,1) = mean(cat(3,ampMaps{indxAz}),3); % Average amplitude;
+    AzimMap(:,:,2) = (phiMaps{indxAz(1)} - phiMaps{indxAz(2)})/2; % From Kalatsky and Stryker, 2003    
+end
 % Elevation:
-if all(idxEl)  
+if all(idxEl)
     disp('Calculating Elevation map...')
-    pow = mean(cat(3,ampMaps{indxEl}),3); % Average amplitude;
-    phi = (phiMaps{indxEl(1)} - phiMaps{indxEl(2)})/2; % From Kalatsky and Stryker, 2003
-    % Package amplitude and phase into complex numbers:
-    ElevMap(:,:) = pow.*(cos(phi) + i.*sin(phi));     
-end       
+    ElevMap(:,:,1) = mean(cat(3,ampMaps{indxEl}),3); % Average amplitude;
+    ElevMap(:,:,2) = (phiMaps{indxEl(1)} - phiMaps{indxEl(2)})/2; % From Kalatsky and Stryker, 2003        
+end
 % Save raw FFT data:
 %%% Save Maps:
 % Save Azimuth map:
-if sum(AzimMap(:)) ~= 0    
-    md = genMetaData(AzimMap,metaData.dim_names([1 2]));
+if sum(AzimMap(:)) ~= 0
+    md = genMetaData(AzimMap,{'Y','X','F'});
     filename = fullfile(SaveFolder, 'AzimuthMap.dat');
     save2Dat(filename,AzimMap,md);
     outFile = [outFile;{filename}];
 end
 % Save Elevation map:
-if sum(ElevMap(:)) ~= 0    
-    md = genMetaData(ElevMap,metaData.dim_names([1 2]));
+if sum(ElevMap(:)) ~= 0
+    md = genMetaData(ElevMap,{'Y','X','F'});
     filename = fullfile(SaveFolder, 'ElevationMap.dat');
     save2Dat(filename,ElevMap,md);
     outFile = [outFile;{filename}];
---
+++ Analysis/Retinotopy/genVSM.m
@@ -0,0 +1,64 @@
+function [data, metaData] = genVSM(SaveFolder, varargin)
+% GENVSM creates a Visual Sign Map (Sereno et al. 1994, Sereno et al. 1995)
+% from the phase component of the Azimuth and Elevation maps created with
+% the function "genRetinotopyMaps.m".
+% Inputs:
+%   SaveFolder (char): full path of the folder containing the files
+%    "AzimuthMap.dat" and "ElevationMap.dat".
+%   opts (optional): structure containing extra parameters. See "default_opts" variable below for details!
+%
+% Outputs:
+%   data (2D matrix): matrix containing the Visual Sign Map (VSM).
+%   metaData (struct): structure containing the meta data associated with
+%   "data".
+
+% Defaults:
+default_opts = struct('SpatialFilter_Sigma', 0);
+opts_values = struct('SpatialFilter_Sigma', [0, Inf]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
+% Notes on Spatial filter:
+% - A value of zero indicates that no filters will apply!
+%%% Arguments parsing and validation %%%
+p = inputParser;
+addRequired(p, 'SaveFolder', @isfolder);
+addOptional(p, 'opts', default_opts,@(x) isstruct(x) && ~isempty(x));
+% Parse inputs:
+parse(p,SaveFolder, varargin{:});
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialize Variables and remove inputParser object:
+SaveFolder = p.Results.SaveFolder;
+opts = p.Results.opts;
+clear p
+%%%%
+% Further input validation:
+% Check if the save folder contains the files "Azimuth.dat" and
+% "Elevation.dat"
+AzFile = fullfile(SaveFolder, 'AzimuthMap.dat');
+ElFile = fullfile(SaveFolder, 'ElevationMap.dat');
+errID = 'umIToolbox:genVSM:MissingInput';
+errMsg = 'The files "AzimuthMap.dat" and "ElevationMap.dat" were not found in the Save Folder!';
+assert(isfile(AzFile) & isfile(ElFile),errID, errMsg);
+% Open Maps
+azMap= loadDatFile(AzFile);
+elMap = loadDatFile(ElFile);
+% Calculate Visual Sign Map:
+phaseAz = azMap(:,:,2); 
+phaseEl = elMap(:,:,2); 
+% Remove NaNs from phase maps:
+phaseAz(isnan(phaseAz)) = 1000;
+phaseEl(isnan(phaseAz)) = 1000;
+disp('Calculating visual sign map...');
+[gradAzx, gradAzy] = gradient(phaseAz);
+[gradElx, gradEly] = gradient(phaseEl);
+gradDirAz = atan2(gradAzy, gradAzx);
+gradDirEl = atan2(gradEly, gradElx);
+
+data = sin(angle(exp(1i.*gradDirAz).*exp(-1i.*gradDirEl)));
+data(isnan(data)) = 0;
+% Filter map
+if opts.SpatialFilter_Sigma > 0
+    data = imgaussfilt(data, opts.SpatialFilter_Sigma);
+end
+% Create meta data:
+metaData = genMetaData(data,{'Y','X'});
+disp('Done');
+end
