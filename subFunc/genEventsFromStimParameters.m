aedab82da209967733d8e2b884c456d232ff7c9f
brunooliveiraferreiradesouza <b.oliveira.ferreira.de.souza@umontreal.ca>
2022-11-08 11:04:11 -0500

    fix: genEventsFromStimParameters: fixed issue #49. There were several errors in the function.

--- subFunc/genEventsFromStimParameters.m
+++ subFunc/genEventsFromStimParameters.m
@@ -34,9 +34,9 @@ eventID = [];
 state = [];
 timestamps = [];
 % eventNameList = {};
-for i = 1:length(stimVar)
-    [ID,chanState,tmstmp] = getEventFromChannel(stimFile.(stimVar{i}), stimFile.Freq);
-    eventID = [eventID;ID.*i];
+for ii = 1:length(stimVar)
+    [ID,chanState,tmstmp] = getEventFromChannel(stimFile.(stimVar{ii}), stimFile.FrameRateHz);
+    eventID = [eventID;ID];
     state = [state;chanState];
     timestamps = [timestamps;tmstmp];    
 end
@@ -53,11 +53,11 @@ function [ID,state,timestamps] = getEventFromChannel(data, FrameRateHz)
 ID = [];state = [];timestamps = [];
 id_list = unique(data(:)); id_list(id_list == 0) = [];
 for i = 1:length(id_list)
-    on_indx = find(data(1:end-1)<.5 & data(2:end)>.5 & data(:,2:end) == id_list(i));
-    off_indx = find(data(1:end-1)>.5 & data(2:end)<.5 & data(:,1:end-1) == id_list(i));
-    timestamps =[timestamps; (sort([on_indx,off_indx]))'./FrameRateHz];
+    on_indx = find(data(1:end-1)<.5 & data(2:end)>.5 & data(2:end) == id_list(i));
+    off_indx = find(data(1:end-1)>.5 & data(2:end)<.5 & data(1:end-1) == id_list(i));
+    timestamps =[timestamps; (sort([on_indx;off_indx]))./FrameRateHz];
     state =[state; repmat([true;false], numel(on_indx),1)];
-    ID = [ID; repmat(id_list(i),2,1)];
+    ID = [ID; repmat(id_list(i),length(state),1)];
 end
 % Rearrange arrays by chronological order:
 [timestamps,idxTime] = sort(timestamps);
