aec2e55e6da387f84e7b7ea11d82c2c6d1fddcab
BrunoOFSouza <bruno.souza@labeotech.com>
2023-05-03 12:24:37 -0400

    dev: Changed HemoCorrection and run_HemoCorrection to use fluo data as input instead of Folder.

--- Analysis/Imaging/run_HemoCorrection.m
+++ Analysis/Imaging/run_HemoCorrection.m
@@ -1,8 +1,14 @@
-function [outData, metaData] = run_HemoCorrection(SaveFolder, varargin)
+function outData = run_HemoCorrection(SaveFolder,data, metaData, varargin)
 % RUN_HEMOCORRECTION calls the function
 % HEMOCORRECTION from the IOI library (LabeoTech).
 % In brief, this function removes hemodynamic fluctuations from any
 % fluorescence signal using the information of two or more channels.
+
+% Inputs:
+%   data: numerical matrix containing image time series (with dimensions "Y", "X", "T").
+%   metaData: .mat file with meta data associated with "data".
+%   opts (optional) : structure containing the Reflectance Channels to be
+%   used in the correction.
 
 % The algorithm used here is described in:
 % Valley, Matthew & Moore, Michael & Zhuang, Jun & Mesa, Natalia & Castelli, Dan & Sullivan,
@@ -17,11 +23,12 @@ default_opts = struct('Red', true, 'Green', true, 'Amber', true);
 opts_values = struct('Red',[false, true], 'Green',[false, true],'Amber',[false, true]);%#ok  % This is here only as a reference for PIPELINEMANAGER.m.
 %%% Arguments parsing and validation %%%
 p = inputParser;
-% Save folder:
 addRequired(p, 'SaveFolder', @isfolder);
+addRequired(p,'data',@(x) isnumeric(x) & ndims(x) == 3); % Validate if the input is a 3-D numerical matrix:
+addRequired(p,'metaData', @(x) isa(x,'matlab.io.MatFile') | isstruct(x)); % MetaData associated to "data".
 addOptional(p, 'opts', default_opts,@(x) isstruct(x) && ~isempty(x));
 % Parse inputs:
-parse(p,SaveFolder, varargin{:});
+parse(p,SaveFolder, data, metaData, varargin{:});
 
 % Translate opts to char cell array:
 fields = fieldnames(p.Results.opts);
@@ -30,9 +37,6 @@ list = fields(idx)';
 
 % Run HemoCorrection function from IOI library:
 disp('Performing hemodynamic correction in fluo channel...')
-outData = HemoCorrection(p.Results.SaveFolder, list);
-tmp = dir(fullfile(p.Results.SaveFolder, 'fluo*.dat'));
-fluoMetaData = load(fullfile(tmp(1).folder, strrep(tmp(1).name, '.dat','.mat')));
-metaData = genMetaData(outData, fluoMetaData.dim_names, fluoMetaData);
+outData = HemoCorrection(p.Results.SaveFolder, p.Results.data, p.Results.metaData, list);
 disp('Finished hemodynamic correction.')
 end
