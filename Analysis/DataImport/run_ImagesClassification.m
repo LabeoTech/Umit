f23dae41475288328b3436b7cfabd14c6857887f
BrunoOFSouza <bruno.souza@labeotech.com>
2023-10-02 16:22:10 -0400

    dev: DataViewer & run_ImagesClassification: migrated Dual-Cam alignment function from DataViewer GUI to run_ImagesClassification. The DataViewer will perform the coregistration only if there is no TFORM file in Labeo's config folder. This will allow the implementation of dual-cam coregistration in batch mode.

--- Analysis/DataImport/run_ImagesClassification.m
+++ Analysis/DataImport/run_ImagesClassification.m
@@ -27,6 +27,41 @@ idxName = ismember({existing_ChanList.name}, default_Output);
 existing_ChanList = existing_ChanList(idxName);
 % Calls function from IOI library. Temporary for now.
 ImagesClassification(RawFolder, SaveFolder, opts.BinningSpatial, opts.BinningTemp,0);
+
+% For Dual-Camera Imaging systems, apply the coregistration using the tform
+% file created in DataViewer's OiS Dual Cam Coregistration utility:
+if isfile(fullfile(SaveFolder,'AcqInfos.mat'))
+    info = load(fullfile(SaveFolder, 'AcqInfos.mat'));
+    if info.AcqInfoStream.MultiCam
+        disp('Dual Camera data found!')
+        % If this is a dual-cam system, look for tform data:
+        % Find tform file:
+        if ispc
+            %     For windows:
+            root = getenv('USERPROFILE');
+        else
+            %     For unix:
+            root = getenv('HOME');
+        end
+        % If the folder doesn't exist, create one:
+        LabeoFolder = fullfile(root,'Documents', 'LabeoTech', 'Config','umIT','tformFiles');
+        % Get the list of tform files:        
+        if isfile(fullfile(LabeoFolder, 'coreg2cam_tform.mat'))
+            % Load tform file and perform coregistration
+            tf = load(fullfile(LabeoFolder, 'coreg2cam_tform.mat'));
+            disp('Applying coregistration to data from camera #2...');
+            [status,warnmsg] = applyTform2Cams(SaveFolder,tf.tform, tf.tformInfo);
+            if ~status
+                warning(['Coregistration failed! Data import will resume without coregistration. ',warnmsg]);
+            else
+                disp('Done!')
+            end
+        else
+            warning('Coregistration TFORM file not found! Data from camera #2 will not be coregistered with camera #1. Data import will resume without coregistration.')
+        end                
+    end
+end
+
 % Get only new files created during ImagesClassification:
 chanList = dir(fullfile(SaveFolder,'*.dat'));
 idx = ismember({chanList.name}, default_Output);
@@ -36,4 +71,5 @@ idxDate = ismember([chanList.datenum], [existing_ChanList.datenum]);
 idxNew = ~all([idxName; idxDate],1);
 chanList = {chanList(idxNew).name};
 outFile = fullfile(SaveFolder, chanList);
+%
 end
