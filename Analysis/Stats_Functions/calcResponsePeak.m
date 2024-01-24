a91f143c4c829b51318a7a491e71f03e45bac9ec
BrunoOFSouza <bruno.souza@labeotech.com>
2024-01-24 15:02:41 -0500

    fix: calcResponsePeak. This function was meant to be in the master branch. Downgraded for compatibility.

--- Analysis/Stats_Functions/calcResponsePeak.m
+++ Analysis/Stats_Functions/calcResponsePeak.m
@@ -16,8 +16,8 @@ function outData = calcResponsePeak(data, varargin)
 
 % Defaults:
 default_Output = 'PeakStats.mat'; %#ok This line is here just for Pipeline management.
-default_opts = struct('STD_threshold', 1, 'ResponsePolarity','positive');
-opts_values = struct('STD_threshold',[eps Inf], 'ResponsePolarity', {{'positive','negative'}});%#ok This is here only as a reference for PIPELINEMANAGER.m.
+default_opts = struct('ResponsePolarity','positive');
+opts_values = struct('ResponsePolarity', {{'positive','negative'}});%#ok This is here only as a reference for PIPELINEMANAGER.m.
 default_object = ''; % This line is here just for Pipeline management to be able to detect this input.
 %%% Arguments parsing and validation %%%
 p = inputParser;
@@ -39,53 +39,40 @@ assert(all(ismember(upper(outData.dim_names), {'O','E','T'})),...
     'Wrong input data type. Data must be a time vector split by event(s).');
 % For each ROI, calculate the average response and use the average time
 % vector to calculate the peak stats:
-ROIdata = cell(1,length(outData.data));
+ROIdata = cell(length(outData.data),1);
 evntList = unique(outData.eventID);
 for ii = 1:length(outData.data)
         
     % Instantiate output arrays:
-    PeakAmp_arr = nan(size(evntList),'single');
-    PeakLat_arr = PeakAmp_arr; onsetAmp_arr = PeakAmp_arr; onsetLat_arr = PeakAmp_arr;
+    PeakAmp_arr = nan(size(evntList),'single');    
     
     for jj = 1:length(evntList)
-        idx = ( outData.eventID == evntList(jj) );
+        idx = ( outData.eventID == evntList(jj) );        
         % Calculate average response to the current event:
         avgResp = squeeze(mean(outData.data{ii}(:,idx,:),2,'omitnan'));
-        if strcmpi(opts.ResponsePolarity, 'negative')
-            % Flip the data around its mean to make the response peak
-            % positive:
-            avgResp = -1.*avgResp + 2*mean(avgResp,1);
-        end
         % Calculate threshold as a multiple of the standard deviation of the 
         %   baseline period from the average response:
         evntFr = round(outData.preEventTime_sec*outData.Freq);
         avgBsln = mean(avgResp(1:evntFr),'omitnan'); % Average baseline amplitude        
-        thr = avgBsln + opts.STD_threshold*std(avgResp(1:evntFr),0,1, 'omitnan');
-        % Find the maximum ('peak') response value that crosses the
-        % threshold:
-        [PeakValue,indxPeak] = max(avgResp(evntFr+1:end),[],1);
-        indxPeak = evntFr + indxPeak;
-        if PeakValue < thr
-            % Skip step when no peak is found
-            continue
+        if strcmpi(opts.ResponsePolarity, 'negative')
+            % Find the minimun('trough') response value that crosses the
+            % threshold:
+            PeakValue = min(avgResp(evntFr+1:end),[],1);
+        else
+            % Find the maximum ('peak') response value that crosses the
+            % threshold:
+            PeakValue = max(avgResp(evntFr+1:end),[],1);
         end
         % Calculate Peak amplitude:
-        PeakAmp_arr(jj) = PeakValue - avgBsln;
-        % Calculate the onset(threshold crossing point) amplitude and latencies in seconds:
-        onsetIndx = evntFr + find(avgResp(evntFr+1:end) > thr,1,'first');
-        % Calculate onset amplitude:
-        onsetAmp_arr(jj) = avgResp(onsetIndx) - avgBsln;
-        % Calculate the Peak latency:
-        PeakLat_arr(jj) = (indxPeak - evntFr)/outData.Freq;
-        % Calculate onset latency:        
-        onsetLat_arr(jj) = (onsetIndx - evntFr)/outData.Freq;
+        PeakAmp_arr(jj) = abs(PeakValue) - avgBsln;
     end
     % Put data inside "ROIdata" structure:
-    ROIdata{ii} = PeakAmp_arr;    
+    ROIdata{ii} = PeakAmp_arr';    
 end
 % Update meta data:
 outData.eventID = evntList;
 outData.label = outData.eventNameList;
 outData.data = ROIdata;
+outData.dim_names = {'O','E'};
 
 end
